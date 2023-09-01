04cd16cf3371007f4c3230d9f1f014739cbe6ccd
mfenton <martin_fenton@hotmail.co.uk>
2023-09-01 16:26:33 +0100

    running API and database for registration

--- backend/api.go
+++ backend/api.go
@@ -9,11 +9,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type ScoreEntry struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Score int    `json:"score"`
-	Time  int    `json:"time"`
+type RegistrationEntry struct {
+	ID        int    `json:"id"`
+	Nickname  string `json:"nickname"`
+	Age       int    `json:"age"`
+	Gender    string `json:"gender"`
+	FirstName string `json:"first name"`
+	LastName  string `json:"last name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
 func setupCORS(w *http.ResponseWriter, req *http.Request) {
@@ -22,22 +26,22 @@ func setupCORS(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-func AddScoreHandler(w http.ResponseWriter, r *http.Request) {
+func AddRegistrationHandler(w http.ResponseWriter, r *http.Request) {
 	// Enable CORS headers for this handler
 	setupCORS(&w, r)
 
 	if r.Method == "POST" {
-		var score ScoreEntry
-		err := json.NewDecoder(r.Body).Decode(&score)
+		var registration RegistrationEntry
+		err := json.NewDecoder(r.Body).Decode(&registration)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		log.Println("Received score:", score.Name, score.Score, score.Time)
+		log.Println("Received score:", registration.Nickname, registration.Age, registration.Gender, registration.FirstName, registration.LastName, registration.Email, registration.Password)
 
-		err = addScoreToDatabase(score.Name, score.Score, score.Time)
+		err = addRegistrationToDatabase(registration.Nickname, registration.Age, registration.Gender, registration.FirstName, registration.LastName, registration.Email, registration.Password)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -46,23 +50,23 @@ func AddScoreHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "GET" {
-		scores, err := getScoresFromDatabase()
+		registrations, err := getRegistrationFromDatabase()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		if len(scores) > 0 {
-			json.NewEncoder(w).Encode(scores)
+		if len(registrations) > 0 {
+			json.NewEncoder(w).Encode(registrations)
 		} else {
-			w.Write([]byte("No scores available"))
+			w.Write([]byte("No registrations available"))
 		}
 	}
 
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to Super Mario API"))
+	w.Write([]byte("Welcome to Real Time Forum API"))
 }
 
 func main() {
@@ -70,7 +74,7 @@ func main() {
 	log.Println("Database initialized successfully")
 
 	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/scores", AddScoreHandler)
+	http.HandleFunc("/registrations", AddRegistrationHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
