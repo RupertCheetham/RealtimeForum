49e2e9aa3bc73f5f7a285b04b8d19657e867a315
michael.adeleke <mikeade08l@gmail.com>
2023-09-25 02:12:57 +0100

    feature: linked post, register and login pages

--- static/js/login.js
+++ static/js/login.js
@@ -1,124 +1,87 @@
 import { setSessionCookie } from "./cookie.js"
-// import { loadLoginScript } from "./registrationToBackend.js"
+import { showRegistrationForm } from "./registration.js"
+import { postsToHTML } from "./postsToHTML.js"
 
 const container = document.getElementById("container")
 
 export function showLoginForm() {
 	let html = `
+		<h1>Login to Forum</h1>
 		<form id="login-form">
 			<div>
-				<h1>Login to Forum</h1>
-			</div>
-			<div class="container">
-				<label for="usernameOrEmail"><b>Username or Email</b></label>
+				<label for="username"><b>Username</b></label>
 				<input
 					type="text"
-					placeholder="Enter Username or Email"
+					placeholder="Enter Username"
 					name="username"
 					required
 					id="usernameOrEmail"
 				/>
-				<br />
-				<p>
-					<label for="password"><b>Password</b></label>
-					<input
-						type="password"
-						placeholder="Enter Password"
-						name="psw"
-						required
-						id="password"
-					/>
-					<br />
-					<button type="submit">Login</button>
-					
-				</p>
 			</div>
-
-			<div class="container" style="background-color: #f1f1f1">
-				<button type="button" class="signup" id="signup">Sign Up</button>
-				<span class="psw">
-					Forgotten
-					<a href="#">password?</a>
-				</span>
+			<div>
+				<label for="password"><b>Password</b></label>
+				<input
+					type="password"
+					placeholder="Enter Password"
+					name="psw"
+					required
+					id="password"
+					/>
+			</div>
+			<div>
+				<button type="submit">Login</button>
+				<button type="button" id="signup">Sign Up</button>
 			</div>
 		</form>
-		<span>
-			<a href="#" id="registration-form">Register Account</a>
-		</span>
-	</div>
  `
 	container.innerHTML = html
-}
 
-// showLoginForm()
-
-// Add event listener to the "Register Account" link
-const registrationForm = document.getElementById("registration-form")
-registrationForm.addEventListener("click", function (event) {
-	event.preventDefault()
-})
-
-const loginForm = document.getElementById("login-form")
-
-// Event listener for switching to the login form
-loginForm.addEventListener("click", function (event) {
-	event.preventDefault()
-})
-
-// Event listener for submitting to login form
-loginForm.addEventListener("submit", function (event) {
-	event.preventDefault()
-
-	const userNameOrEmail = document.getElementById("usernameOrEmail").value
-	const password = document.getElementById("password").value
-
-	console.log(userNameOrEmail, password)
-
-	fetch("http://localhost:8080/login", {
-		method: "POST",
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			username: userNameOrEmail,
-			password: password,
-		}),
+	// Add event listener to the "Register Account" link
+	const signup = document.getElementById("signup")
+	signup.addEventListener("click", function (event) {
+		event.preventDefault()
+		showRegistrationForm()
 	})
-		.then((response) => {
-			if (response.ok) {
-				return response.json()
-			} else {
-				throw new Error("POST request failed!")
-			}
+
+	const loginForm = document.getElementById("login-form")
+	// Event listener for submitting to login form
+	loginForm.addEventListener("submit", function (event) {
+		event.preventDefault()
+
+		const userNameOrEmail = document.getElementById("usernameOrEmail").value
+		const password = document.getElementById("password").value
+
+		console.log(userNameOrEmail, password)
+
+		fetch("http://localhost:8080/login", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: userNameOrEmail,
+				password: password,
+			}),
 		})
-		.then((data) => {
-			console.log(data.message)
-			if (data.message === "Login successful") {
-				setSessionCookie()
-			}
-		})
-		.catch((error) => {
-			console.log(error)
-		})
-})
-
-const signupButton = document.getElementById("signup")
-
-signupButton.addEventListener("click", function (event) {
-	event.preventDefault()
-
-	// Load or serve your registrationToBackend.js script here
-	loadRegistrationToBackendScript()
-})
-
-function loadRegistrationToBackendScript() {
-	// Create a script element
-	const script = document.createElement("script")
-
-	// Set the src attribute to your registrationToBackend.js file
-	script.src = "../static/js/registrationToBackend.js"
-
-	// Append the script element to the document's head
-	document.head.appendChild(script)
+			.then((response) => {
+				if (response.ok) {
+					return response.json()
+				} else {
+					throw new Error("POST request failed!")
+				}
+			})
+			.then((data) => {
+				console.log(data.message)
+				if (data.message === "Login successful") {
+					setSessionCookie()
+					postsToHTML()
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	})
 }
+
+showLoginForm()
--- static/js/postsToHTML.js
+++ static/js/postsToHTML.js
@@ -1,50 +1,82 @@
-async function postsToHTML() {
-  const response = await fetch('http://localhost:8080/posts');
-  const postContainer = document.getElementById('postContainer');
-  const posts = await response.json();
+const container = document.getElementById("container")
 
-  for (const post of posts) {
-    const postElement = document.createElement('div');
-    postElement.id = 'Post' + post.id;
-    postElement.classList.add('post');
+export async function postsToHTML() {
+	let html = `
+    <h1>Post</h1>
+    <form id="post-form">
+    <div>
+      <p>Kindly fill in this form to post.</p>
+      <label for="post"><b>Post</b></label>
+      <input type="text" placeholder="Enter Message" name="postText" id="postText" required /><br>
+      <label for="categories"><b>Categories</b></label>
+      <input type="text" placeholder="Enter Categories" name="categories" id="categories" required /><br>
+      <label for="image"><b>Image</b></label>
+      <input type="text" placeholder="Enter Image String" name="image" id="image" required /><br>
+      <button type="submit" id="submit">Submit Post</button>
+    </div>
+    </form>
+    <br>
+    <form id="comment-form">
+    <h1>Comment</h1>
+    <div>
+      <p>Kindly fill in this form to comment.</p>
+      <label for="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="commentUsername" id="commentUsername" required /><br>
+      <label for="parentPostID"><b>parentPostID</b></label>
+      <input type="text" placeholder="Enter parentPostID" name="parentPostID" id="parentPostID" required /><br>
+      <label for="commentText"><b>Comment</b></label>
+      <input type="text" placeholder="Enter comment" name="commentText" id="commentText" required /><br>
+      <button type="submit" id="submit">Submit Comment</button>
+    </div>
+    </form>
+  `
 
-    const comments = await fetchComments(post.id); // Wait for the comments to be fetched
-    console.log(comments);
+	container.innerHTML = html
 
-    postElement.textContent = `
+	const response = await fetch("http://localhost:8080/posts")
+	const postContainer = document.getElementById("postContainer")
+	const posts = await response.json()
+
+	for (const post of posts) {
+		const postElement = document.createElement("div")
+		postElement.id = "Post" + post.id
+		postElement.classList.add("post")
+
+		const comments = await fetchComments(post.id) // Wait for the comments to be fetched
+		console.log(comments)
+
+		postElement.textContent = `
       Id: ${post.id},
       Username: ${post.username},
       Img: ${post.img},
       Body: ${post.body},
       Categories: ${post.categories},
       Reaction: ${post.reaction},
-    `;
+    `
 
-  
-    if (comments.length > 0) {
-      const commentsContainer = document.createElement('div');
-      commentsContainer.id = 'commentContainer';
-      let commentsNum = 1
-      comments.forEach(comment => {
-        const commentElement = document.createElement('div');
-        commentElement.className = 'comment' + commentsNum++;
-        commentElement.textContent = `Comment: ${comment.body}`;
-        commentsContainer.appendChild(commentElement);
-      });
+		if (comments.length > 0) {
+			const commentsContainer = document.createElement("div")
+			commentsContainer.id = "commentContainer"
+			let commentsNum = 1
+			comments.forEach((comment) => {
+				const commentElement = document.createElement("div")
+				commentElement.className = "comment" + commentsNum++
+				commentElement.textContent = `Comment: ${comment.body}`
+				commentsContainer.appendChild(commentElement)
+			})
 
-      postElement.appendChild(commentsContainer);
-    }
+			postElement.appendChild(commentsContainer)
+		}
 
-    postContainer.appendChild(postElement);
-  }
+		postContainer.appendChild(postElement)
+	}
 }
+postsToHTML()
 
-  // Comments need to be reworked, currently very inefficient.  Probably foreign keys will be involved
+// Comments need to be reworked, currently very inefficient.  Probably foreign keys will be involved
 async function fetchComments(parentPostID) {
-  const response = await fetch('http://localhost:8080/comments');
-  const comments = await response.json();
-  console.log("comments:", comments)
-  return comments.filter(comment => comment.parentPostId == parentPostID);
+	const response = await fetch("http://localhost:8080/comments")
+	const comments = await response.json()
+	console.log("comments:", comments)
+	return comments.filter((comment) => comment.parentPostId == parentPostID)
 }
-
-postsToHTML();
