089fc465614e2e1e3d5326f2fdd6e1838eb5b4de
mfenton <martin_fenton@hotmail.co.uk>
2023-09-25 13:19:05 +0100

    partial mainpage

--- static/js/postsToBackend.js
+++ static/js/postsToBackend.js
@@ -1,5 +1,36 @@
 /* The code is adding an event listener to the form with the id "post-form". When the form is
 submitted, the event listener function is executed. */
+
+let html = `
+
+<form id="post-form">
+    <div>
+      <h1>Post</h1>
+      <p>Kindly fill in this form to post.</p>
+      <!-- label and input for postText -->
+      <label for="post"><b>Post</b></label>
+      <input type="text" placeholder="Enter Message" name="postText" id="postText" required /><br>
+      <!-- label and input for categories -->
+      <label for="categories"><b>Categories</b></label>
+      <input type="text" placeholder="Enter Categories" name="categories" id="categories" required /><br>
+      <!-- label and input for img -->
+      <label for="image"><b>Image</b></label>
+      <input type="text" placeholder="Enter Image String" name="image" id="image" required /><br>
+      <!-- submit button -->
+
+      <button type="submit" id="submit">Submit Post</button>
+    </div>
+    <!-- wrapping the text inside the p tag with a tag for routing to the login page URL-->
+    <!-- the # must ideally be replaced by the login page URL -->
+    <div>
+
+  </form>
+
+ `
+container.innerHTML = html
+
+function postsToBackend() {
+
 const postForm = document.getElementById("post-form")
 
 postForm.addEventListener("submit", function (event) {
@@ -27,3 +58,4 @@ postForm.addEventListener("submit", function (event) {
 	})
 	console.log("bottom of page")
 })
+}
