811b8e2fab25c89a1f0acae7365a3d12a9e068c5
Michael A <{ID}+{username}@users.noreply.github.com>
2023-10-23 16:46:29 +0100

    refactor: changed from creationdate and expirationdate to be creationtime and expirationtime

--- backend/db/dbCookie.go
+++ backend/db/dbCookie.go
@@ -1,13 +1,14 @@
 package db
 
 import (
+	"database/sql"
 	"realtimeForum/utils"
 	"time"
 )
 
 // creates a session
-func CreateSession(userId int, expirationDate time.Time) (session Session, err error) {
-	statement := `INSERT INTO Cookies (SessionID, UserID, CreationDate, ExpirationDate) values (?, ?, ?, ?) returning SessionID, UserID, CreationDate, ExpirationDate`
+func CreateSession(userId int, expirationTime time.Time) (session Session, err error) {
+	statement := `INSERT INTO Cookies (SessionID, UserID, CreationTime, ExpirationTime) values (?, ?, ?, ?) returning SessionID, UserID, CreationTime, ExpirationTime`
 
 	stmt, err := Database.Prepare(statement)
 	utils.HandleError("session error:", err)
@@ -17,25 +18,25 @@ func CreateSession(userId int, expirationDate time.Time) (session Session, err e
 	UUID := utils.GenerateNewUUID()
 	timeNow := time.Now()
 
-	err = stmt.QueryRow(UUID, userId, timeNow, expirationDate).Scan(&session.SessionID, &session.UserId, &session.CreationDate, &session.ExpirationTime)
+	err = stmt.QueryRow(UUID, userId, timeNow, expirationTime).Scan(&session.SessionID, &session.UserId, &session.CreationTime, &session.ExpirationTime)
 	return
 }
 
-// // GetSessionByToken retrieves a session by its session token from the database.
-// func GetSessionByToken(sessionToken string) (*Session, error) {
-// 	var session Session
+// GetSessionByToken retrieves a session by its session token from the database.
+func GetSessionByToken(sessionToken string) (*Session, error) {
+	var session Session
 
-// 	query := "SELECT UserID, ExpirationTime FROM sessions WHERE SessionToken = ?"
-// 	err := Database.QueryRow(query, sessionToken).Scan(&session.UserId, &session.ExpirationTime)
+	statement := "SELECT UserID, ExpirationTime FROM sessions WHERE SessionToken = ?"
+	err := Database.QueryRow(statement, sessionToken).Scan(&session.UserId, &session.ExpirationTime)
 
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			return nil, nil // Session not found
-// 		}
-// 		return nil, err // Other error
-// 	}
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // Session not found
+		}
+		return nil, err // Other error
+	}
 
-// 	session.SessionID = sessionToken
+	session.SessionID = sessionToken
 
-// 	return &session, nil
-// }
+	return &session, nil
+}
