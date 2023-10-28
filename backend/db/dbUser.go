503a46ec5ecca0beda2615e81b07c660264909e1
Michael A <{ID}+{username}@users.noreply.github.com>
2023-10-28 18:24:11 +0100

    tidyup: removed comments from dbuser

--- backend/db/dbUser.go
+++ backend/db/dbUser.go
@@ -12,7 +12,6 @@ func AddUserToDatabase(username string, age int, gender string, firstName string
 	_, err := Database.Exec("INSERT INTO USERS (Username, Age, Gender, First_name, Last_name, Email, Password) VALUES (?, ?, ?, ?, ?, ?, ?)", username, age, gender, firstName, lastName, email, password)
 	if err != nil {
 		utils.HandleError("Error adding USER to database:", err)
-		// log.Println("Error adding USER to database:", err)
 	}
 	return err
 }
@@ -21,7 +20,6 @@ func GetUsersFromDatabase() ([]UserEntry, error) {
 	rows, err := Database.Query("SELECT Username, Age, Gender, First_name, Last_name, Email, Password FROM USERS ORDER BY Id ASC")
 	if err != nil {
 		utils.HandleError("Error querying USERS from database in GetUsersFromDatabase:", err)
-		log.Println("Error querying USERS from database in GetUsersFromDatabase:", err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -32,7 +30,6 @@ func GetUsersFromDatabase() ([]UserEntry, error) {
 		err := rows.Scan(&entry.Username, &entry.Age, &entry.Gender, &entry.FirstName, &entry.LastName, &entry.Email, &entry.Password)
 		if err != nil {
 			utils.HandleError("Error scanning row from database in GetUsersFromDatabase:", err)
-			log.Println("Error scanning row from database in GetUsersFromDatabase:", err)
 			return nil, err
 		}
 		users = append(users, entry)
@@ -42,9 +39,6 @@ func GetUsersFromDatabase() ([]UserEntry, error) {
 }
 
 func GetUsernameFromSessionID(sessionID string) string {
-	// Initialize a database connection. Ensure you have a valid database connection setup.
-
-	// SQL query to retrieve the username associated with the provided SessionID
 	query := "SELECT u.Username FROM COOKIES AS c INNER JOIN USERS AS u ON c.UserID = u.Id WHERE c.SessionID = ?"
 
 	// Execute the query and retrieve the username
@@ -62,7 +56,6 @@ func FindUserFromDatabase(username string) (UserEntry, error) {
 	rows, err := Database.Query("SELECT * FROM USERS WHERE Username = ?", username)
 	if err != nil {
 		utils.HandleError("Error querying USERS from database in FindUserFromDatabase:", err)
-		// log.Println("Error querying USERS from database in FindUserFromDatabase:", err)
 		return UserEntry{}, err
 	}
 	defer rows.Close()
@@ -87,7 +80,6 @@ func DeleteUserFromDatabase(username string) error {
 	_, err := Database.Exec("DELETE FROM USERS WHERE Username = ?", username)
 	if err != nil {
 		utils.HandleError("Error querying USERS from database in DeleteUserFromDatabase:", err)
-		// log.Println("Error deleting USER from database in DeleteUserFromDatabase:", err)
 	} else {
 		utils.WriteMessageToLogFile("User " + username + " delete")
 		fmt.Println("User deleted")
@@ -99,7 +91,6 @@ func DeleteAllUsersFromDatabase() error {
 	_, err := Database.Exec("DELETE FROM USERS")
 	if err != nil {
 		utils.HandleError("Error querying USERS from database in DeleteUserFromDatabase:", err)
-		// log.Println("Error deleting USERS from database in DeleteAllUsersFromDatabase:", err)
 	} else {
 		utils.WriteMessageToLogFile("All users delete from user table")
 		fmt.Println("All users deleted")
