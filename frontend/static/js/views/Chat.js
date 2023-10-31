b36e1c0fd6c0e7a8ddf4100cb23707259b0d5329
Ardek <cheethamthing@gmail.com>
2023-10-31 12:24:21 +0000

    Feat: Chat now submits messages when enter is pressed

--- frontend/static/js/views/Chat.js
+++ frontend/static/js/views/Chat.js
@@ -82,11 +82,11 @@ export default class Chat extends AbstractView {
       const Recipient = await usernameFromUserID(RecipientID);
       const chatTextBox = this.getChatTextBoxHTML();
       chatContainer.innerHTML = `
-			
-			<h1 id="recipient" class = "chat-font"> ${Recipient}</h1>
-
+      <div class = "allChat"
+      <h1 id="recipient" class = "chat-font"> ${Recipient}</h1>
 			<div id="chatHistory"></div>
 			${chatTextBox}
+      </div>
 			`;
       await this.webSocketChat();
     }
@@ -168,30 +168,31 @@ export default class Chat extends AbstractView {
 
     });
 
-    //deals with sending new messages to the backend when sendButton is clicked
-    document.getElementById("sendButton").addEventListener("click", () => {
-      const messageInput = document.getElementById("messageInput");
-      // Get the value of the input and trim leading/trailing spaces
-      const Message = messageInput.value.trim();
-
-      if (Message !== "") {
-        // Check if the message contains at least one non-space character
-        if (/\S/.test(Message)) {
-          console.log("Sending message:", Message);
-          // Send the message to the server via WebSocket
-          socket.send(
-            JSON.stringify({
-              type: "chat",
-              body: Message,
-              sender: Sender,
-              recipient: Recipient,
-            })
-          );
-          // Clear the input field
-          messageInput.value = "";
-        }
+    //deals with sending new messages to the backend when sendButton is clicked or enter is pressed
+    document.getElementById("sendButton").addEventListener("click", sendMessage);
+    document.getElementById("messageInput").addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        sendMessage();
       }
     });
+
+    function sendMessage() {
+      const messageInput = document.getElementById("messageInput");
+      const Message = messageInput.value.trim();
+
+      if (Message !== "" && /\S/.test(Message)) {
+        console.log("Sending message:", Message);
+        socket.send(
+          JSON.stringify({
+            type: "chat",
+            body: Message,
+            sender: Sender,
+            recipient: Recipient,
+          })
+        );
+        messageInput.value = "";
+      }
+    }
   }
 
   // displays chat history (if any) between two users
@@ -312,33 +313,30 @@ export default class Chat extends AbstractView {
     // split up the time, ommiting seconds
     const time = timePart.split(":"); // Split the time into hours, minutes, and seconds
 
-    const hours = parseInt(time[0], 10);
-    const minutes = parseInt(time[1], 10);
+    const hours = time[0]
+    const minutes = time[1]
 
     // Split up the date
-    const dateParts = datePart.split("-");
+    const dateParts = datePart.split('-');
+    const day = dateParts[0];
+    const month = dateParts[1]
+    const year = dateParts[2];
 
-    const day = parseInt(dateParts[0], 10);
-    const month = parseInt(dateParts[1], 10) - 1; // Months are 0-based in JavaScript
-    const year = parseInt(dateParts[2], 10);
-
-    let currentTime = `${hours}:${minutes}`;
-    let currentDate = `${day}/${month}/${year}`;
+    let currentTime = `${hours}:${minutes}`
+    let currentDate = `${day}/${month}/${year}`
     let formattedTimestamp = `${hours}:${minutes} ${day}/${month}/${year}`;
 
     if (this.previousTime == currentTime && this.previousDate == currentDate) {
-      formattedTimestamp = "";
-    } else if (
-      this.previousTime != currentTime &&
-      this.previousDate == currentDate
-    ) {
+      formattedTimestamp = ''
+    } else if ((this.previousTime != currentTime && this.previousDate == currentDate)) {
       formattedTimestamp = `${hours}:${minutes}`;
     } else {
-      formattedTimestamp = `${hours}:${minutes} ${day}/${month}/${year}`;
+      formattedTimestamp = `${hours}:${minutes} ${day}/${month}/${year}`
     }
 
-    this.previousTime = currentTime;
-    this.previousDate = currentDate;
+
+    this.previousTime = currentTime
+    this.previousDate = currentDate
     return formattedTimestamp;
   }
 }
