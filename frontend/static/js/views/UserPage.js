55d3fd8dc90945a21c32a9a031becc73ae983efb
michael.adeleke <mikeade08l@gmail.com>
2023-11-12 18:46:51 +0000

    refactor: rendering posts

--- frontend/static/js/views/UserPage.js
+++ frontend/static/js/views/UserPage.js
@@ -1,10 +1,6 @@
 import AbstractView from "./AbstractView.js"
 import Nav from "./Nav.js"
-import {
-	attachCommentForm,
-	attachCommentsToPost,
-	createCloseCommentButton,
-} from "./Comments.js"
+import { renderPosts } from "./Post.js"
 const nav = new Nav()
 
 export default class UserPage extends AbstractView {
@@ -31,87 +27,10 @@ export default class UserPage extends AbstractView {
 		}
 
 		const posts = await response.json()
-		const username = localStorage.getItem("username")
 		const postsContainer = document.getElementById("postsContainer")
 		postsContainer.innerHTML = ""
 
-		console.log("posts:", posts)
-
-		for (const post of posts) {
-			//makes the container for the individual posts and all its contents
-			let postContainer = document.createElement("div")
-			postContainer.id = "postContainer" + post.id
-			postContainer.classList.add("postContainer")
-			let postElement = document.createElement("div")
-			postElement.id = "Post" + post.id
-			postElement.classList.add("post")
-			postElement.setAttribute("reactionID", post.reactionID)
-			let dateObj = new Date(post.creationDate)
-			let month = dateObj.getUTCMonth() + 1
-			let day = dateObj.getUTCDate()
-			let year = dateObj.getUTCFullYear()
-			// let time = dateObj.getTime()
-			let hour = dateObj.getUTCHours()
-			let mins = dateObj.getUTCMinutes()
-			let secs = dateObj.getUTCSeconds()
-
-			let newdate = `${day}/${month}/${year} ${hour}:${mins}:${secs} `
-
-			postElement.innerHTML = `
-      <ul>
-			<li><b>Date: </b>${newdate}</li>
-        <li><b>Username:</b>${post.username}</li>
-        <li><b>Img:</b> ${post.img}</li>
-        <li><b>Body:</b> ${post.body}</li>
-        <li><b>Categories:</b> ${post.categories}</li>
-        <button class="reaction-button" reaction-parent-class="post" reaction-parent-id="${
-					post.id
-				}" reaction-action="like" reaction-id = "${postElement.getAttribute(
-				"reactionID"
-			)}">👍 ${post.postLikes}</button>
-        <button class="reaction-button" reaction-parent-class="post" reaction-parent-id="${
-					post.id
-				}" reaction-action="dislike" reaction-id = "${postElement.getAttribute(
-				"reactionID"
-			)}">👎 ${post.postDislikes}</button>
-        </li>
-      </ul>
-      `
-
-			// attaches the comment form to the bottom of each post
-			attachCommentForm(post, postElement)
-
-			// appends the post(element) to the post Box
-			postContainer.appendChild(postElement)
-
-			// makes the commentsContainer, that comments (if any) will be appended to
-			const commentsContainer = document.createElement("div")
-			commentsContainer.id = "commentsContainer" + post.id
-			commentsContainer.className = "commentsContainer"
-			postContainer.appendChild(commentsContainer)
-			commentsContainer.style.display = "none"
-			const closeCommentButton = createCloseCommentButton(commentsContainer)
-			// attaches closeComment Button to bottom of commentscontainer
-			postContainer.appendChild(closeCommentButton)
-			closeCommentButton.style.display = "none"
-
-			postElement.addEventListener("click", () => {
-				if (commentsContainer.style.display === "none") {
-					commentsContainer.style.display = "block"
-					// if (commentsContainer.querySelectorAll("div").length != 0) {
-					closeCommentButton.style.display = "block"
-					// }
-				}
-			})
-
-			let comments = post.comments // Wait for the comments to be fetched
-
-			if (comments !== null) {
-				attachCommentsToPost(commentsContainer, comments)
-			}
-
-			postsContainer.appendChild(postContainer)
-		}
+		renderPosts(posts, postsContainer)
 	}
 
 	async Logout() {
